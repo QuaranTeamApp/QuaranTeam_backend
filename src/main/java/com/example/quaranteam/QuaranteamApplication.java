30d3c4f3ef318c1b8b94783ed0db83feb40a8f18
chris-dmello <c.d.dmello@gmail.com>
2020-09-20 12:03:15 -0500

    Initial project commit

---
+++ src/main/java/com/example/quaranteam/QuaranteamApplication.java
@@ -0,0 +1,13 @@
+package com.example.quaranteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaranteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaranteamApplication.class, args);
+	}
+
+}
