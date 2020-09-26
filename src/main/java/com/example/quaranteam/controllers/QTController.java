3cd7392690ea0a1bd917ae90876e5372258b7a02
chris-dmello <c.d.dmello@gmail.com>
2020-09-26 12:38:36 -0500

    added add

--- src/main/java/com/example/quaranteam/controllers/QTController.java
+++ src/main/java/com/example/quaranteam/controllers/QTController.java
@@ -1,7 +1,10 @@
 package com.example.quaranteam.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class QTController {
@@ -11,4 +14,8 @@ public class QTController {
         return "Success!";
     }
 
+    @GetMapping(value="/add")
+    public int getMethodName(@RequestParam int x, @RequestParam int y) {
+        return x+y;
+    }
 }
